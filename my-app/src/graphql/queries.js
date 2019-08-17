296d7e0de104e92d12e1ba47e7aba185b3cd2d87
Mat Werber <werberm@amazon.com>
2019-08-17 17:55:48 -0400

    update

--- my-app/src/graphql/queries.js
+++ my-app/src/graphql/queries.js
@@ -1,17 +1,5 @@
 // eslint-disable
 // this is an auto generated file. This will be overwritten
-/*
-export const listCards = `query ListCards {
-    listCards {
-        card_id
-        front_text
-        back_text
-        back_audio
-    }
-}
-`;
-*/
-
 export const getCard = `query getCard($card_id: ID!) {
     listCards(card_id: $card_id) {
         card_id,
@@ -22,23 +10,7 @@ export const getCard = `query getCard($card_id: ID!) {
 }
 `;
 
-export const listTodos = `query ListTodos(
-    $filter: ModelTodoFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        name
-        description
-      }
-      nextToken
-    }
-  }
-  `;
-
-  export const listCards = `query ListCards(
+export const listCards = `query ListCards(
     $filter: ModelCardFilterInput
     $limit: Int
     $nextToken: String
